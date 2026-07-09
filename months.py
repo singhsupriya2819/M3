b3cb2c68c043c7ec26bd5d982f579f6eed8f12c1
singhsupriya2819 <singhsupriya2819@gmail.com>
2026-07-09 17:10:07 +0530

    motnhs

---
+++ months.py
@@ -0,0 +1,9 @@
+months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+]
+
+print("Months of the Year:")
+for month in months:
+    print(month)
